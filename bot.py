84bf0eaaac11134365874a323ac67c105813c63f
PC <pc@kvalvik.no>
2015-12-26 19:12:34 +0100

    Added osu! functionality.
    Users can now !rank <profile>
    Alternatively, get specific values using !osu <profile> <stat>

--- bot.py
+++ bot.py
@@ -10,12 +10,16 @@ if len(sys.argv) < 3:
 
 client.login(sys.argv[1], sys.argv[2])
 
+osu_api = input("Enter a valid osu! API key for osu! functions (enter nothing to disable): ")  # API Key for osu!
+
 usage = {
     "!pcbot": "display commands",
     "!google <query ...>": "search the web",
     "!display <query ...>": "search the web for images",
     "!lucky <query ...>": "retrieve a link",
-    "!profile <user>": "sends link to osu! profile"
+    "!profile <user>": "sends link to osu! profile",
+    "!osu <profile> <stat>": "displays a profile stat (may break some things",
+    "!rank <profile>:": "displays various stats for profile"
 }
 
 
@@ -28,8 +32,7 @@ def longest_cmd():
     return cmd_len
 
 
-@client.event
-def on_message(message):
+def handle_command(message):
     args = message.content.split()
     send_message = ""
 
@@ -54,27 +57,79 @@ def on_message(message):
             send_message = result.url
         else:
             send_message = ":thumbsdown:"
-    elif args[0] == "!profile":
+    elif args[0] == "!profile":  # Link to osu! profile
         if len(args) > 1:
             send_message = r"http://osu.ppy.sh/u/" + str(args[1])
         else:
             send_message = ":thumbsdown:"
+    elif args[0] == "!osu":  # Give osu! profile stat
+        if len(args) > 2:
+            if osu_api:
+                to_get = r"http://osu.ppy.sh/api/get_user?k=" + osu_api + r"&u=" + args[1]
+                osu_stats_request = requests.get(to_get)
+                osu_stats = osu_stats_request.json()[0]
+                try:
+                    send_message = "**%s** for %s: `%s`" % (args[2], args[1], osu_stats[args[2]])
+                except KeyError:
+                    send_message = "No such key. :thumbsdown:"
+            else:
+                send_message = "This command is disabled. :thumbsdown:"
+        else:
+            send_message = ":thumbsdown:"
+    elif args[0] == "!rank":  # Give a list of osu! profile stats
+        if len(args) > 1:
+            if osu_api:
+                to_get = r"http://osu.ppy.sh/api/get_user?k=" + osu_api + r"&u=" + args[1]
+                osu_stats_request = requests.get(to_get)
+                osu_stats = osu_stats_request.json()[0]
+                send_message = "*Stats for %s* (%s) ```" % (
+                    osu_stats["username"],
+                    osu_stats["user_id"]
+                )
+                send_message += "Performance:   %spp (#%s) /#%s" % (
+                    osu_stats["pp_raw"],
+                    osu_stats["pp_rank"],
+                    osu_stats["pp_country_rank"]
+                )
+                send_message += "\nAccuracy:      %0.6f" % float(osu_stats["accuracy"])
+                send_message += "\n               %s SS %s S %s A" % (
+                    osu_stats["count_rank_ss"],
+                    osu_stats["count_rank_s"],
+                    osu_stats["count_rank_a"]
+                )
+                send_message += "\nPlaycount:     " + osu_stats["playcount"]
+                send_message += "```"
+            else:
+                send_message = "This command is disabled. :thumbsdown:"
+        else:
+            send_message = ":thumbsdown:"
     elif args[0] == "!pcbot":  # Show help
-        help_message = "Commands: ```"
+        send_message = "Commands: ```"
         space_len = longest_cmd() + 4
         for k, v in usage.items():
-            help_message += "\n" + k + " "*(space_len - len(k)) + v
-        help_message += "```"
-        send_message = help_message
+            send_message += "\n" + k + " "*(space_len - len(k)) + v
+        send_message += "```"
 
-    # Send message if received command
+    return send_message
+
+
+@client.event
+def on_message(message):
+    send_message = handle_command(message)
     if send_message:
         client.send_message(message.channel, message.author.mention() + " " + send_message)
 
 
 @client.event
+def on_message_edit(before, after):
+    send_message = handle_command(after)
+    if send_message:
+        client.send_message(after.channel, after.author.mention() + " " + send_message)
+
+
+@client.event
 def on_ready():
-    print('Logged in as')
+    print('\nLogged in as')
     print(client.user.name)
     print(client.user.id)
     print('------')
